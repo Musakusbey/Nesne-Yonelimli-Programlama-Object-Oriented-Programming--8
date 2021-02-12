f8cc28afc24b36cb5d9bfb260477edd9f1ad24fa
Musa Kuşbey <78588618+Musakusbey@users.noreply.github.com>
2021-02-12 15:47:32 +0300

    Add files via upload
    Nesne Yonelimli Programlama (Object-Oriented Programming)-- 8->Setter ve Getter(private ulaşmak)

---
+++ 8.Private nasıl ulaşılır.(Set ve Get).cpp
@@ -0,0 +1,184 @@
+/*#include <iostream>
+
+using namespace std;
+
+class Employee
+{
+	private:
+		
+		string name;
+		
+	public:
+		
+		void setName(string isim)
+		{
+			name=isim;
+		}
+		
+		string getName()
+		{
+			return name;
+			
+			
+		}
+};	
+
+int main ()
+{
+	Employee employee;
+	
+	employee.setName("musa");
+	
+	
+	cout<<employee.getName() << endl;
+	
+	return 0;
+} */
+
+/*#include<iostream>
+
+using namespace std;
+
+class Employee
+{
+	private:
+		
+		string name;
+		
+		int age;
+		
+	public:
+		
+		void setx(string isim)
+		{
+			name=isim;
+		}
+		
+		string getx()
+		{
+			return name;
+		}
+		
+		void sety(int yas)
+		{
+			age=yas;
+		}
+		
+		int gety()
+		{
+			return age;
+		}
+	
+};
+
+int main ()
+{  
+    Employee employee;
+    
+    employee.setx("Musa");
+    
+    employee.sety(18);
+    
+    cout<<employee.getx()<<endl;
+    
+    cout<<employee.gety()<<endl;
+	
+	return 0;
+} */  
+
+ /*#include<iostream>
+
+using namespace std;
+
+class Employee
+{
+	private:
+		
+		string name;
+		
+		int age;
+		
+	public:
+		
+		void setAge(int yas)
+		{
+			if(yas<0)
+			{
+				cout<<"Boyle bir deger giremezsiniz:"<<endl;
+			}
+			
+			else
+			{
+				age=yas;
+			}
+    	}
+			
+			int getAge()
+			{
+				return age;
+			}
+		
+		
+		void setName(string isim)
+		{
+			name=isim;
+		}
+		
+		string getName()
+		{
+			return name;
+		}
+};
+   
+ int main ()
+ {
+ 	Employee *employee=new Employee;
+ 	
+ 	employee->setName("MUSA");
+ 	
+ 	employee->setAge(18);
+ 	
+ 	cout<<employee->getName()<<endl;
+ 	
+ 	cout<<employee->getAge()<<endl;
+ 	
+ 	return 0;
+ }
+   */
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   	
+   	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
